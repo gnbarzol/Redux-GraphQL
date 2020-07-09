19de2110450d99828c3de5ec4aaec578659ee86a
Gary Barzola <gnbarzol@espol.edu.ec>
2020-07-09 11:47:41 -0500

    Initial commit, structure

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../assets/styles/App.css';
+
+function App() {
+  return (
+    <div>
+      <header >
+        <p>Hola</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
