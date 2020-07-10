e38cbdeb9d14493edcd448680ab55ef93dfdc45a
Gary Barzola <gnbarzol@espol.edu.ec>
2020-07-10 16:19:18 -0500

    Creating middleware for safe routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,19 +1,33 @@
 import React from 'react';
-import { Switch, Route} from 'react-router-dom';
+import { Switch, Route, Redirect} from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Home from '../components/home';
 import FavPage from '../components/favPage';
 import Login from '../components/login';
 
-const Routes = () => {
+const PrivateRoute = ({path, component, loggedIn, ...rest}) => {
+    if(loggedIn)
+        return <Route path={path} component={component} {...rest} />;
+    alert('Inicia sesión para ver este apartado');
+    return <Redirect to='/login' {...rest}/>;
+}
+
+const Routes = ({loggedIn}) => {
     return (
     // <BrowserRouter>
         <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route exact path='/favs' component={FavPage}/>
-            <Route exact path='/login' component={Login}/>
+            <Route exact path='/' component={Home} loggedIn={loggedIn}/>
+            <PrivateRoute path='/favs' component={FavPage} loggedIn={loggedIn}/>
+            <Route path='/login' component={Login}/>
         </Switch>
     // </BrowserRouter>
 )}
 
-export default Routes;
+const mapStateToProps = (state) => {
+    return {
+        loggedIn: state.user.loggedIn,
+    }
+}
+
+export default connect(mapStateToProps, null)(Routes);
